56210cea634ca3bfce9e55961b22f95dcd016fb8
jmaltedo <dohrendorfjm@aol.de>
2021-10-04 20:37:15 +0200

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,23 @@ if ('serviceWorker' in navigator){
     .then((reg) => console.log('service worker registered', reg))
     .catch((err) => console.log('service worker not registered', err));
 }
+
+
+const start = document.querySelector("#start");
+const stop = document.querySelector("#stop");
+
+const coordinates = [];
+
+start.addEventListener("click", () => {
+  navigator.geolocation.watchPosition(
+    data => {
+      console.log(data);
+      coordinates.push([data.coords.latitude, data.coords.longitude, data.timestamp]);
+      document.getElementById('coordinates').innerHtml = data;
+      // window.localStorage.setItem("coordinates", JSON.stringify(coordinates));
+    },
+    error => console.log(error), {
+      enableHighAccuracy: true
+    }
+  );
+});
