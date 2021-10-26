ec5b76ac31a4594690c3de289d31b215debf2403
jmaltedo <dohrendorfjm@aol.de>
2021-10-27 00:19:01 +0200

    initial commit

--- src/js/sw-install.js
+++ src/js/sw-install.js
@@ -1,6 +1,5 @@
 
-// Service Worker
-
+// Register Service Worker
 if ('serviceWorker' in navigator){
   navigator.serviceWorker.register('service-worker.js')
     .then((reg) => console.log('service worker registered', reg))
